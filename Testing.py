eb42d21bc676de27626a13a6633819c7de96a20e
Bryant Nguyen <bryanthn@uci.edu>
2020-02-01 11:47:19 -0800

    Added Deck mechanics

--- Testing.py
+++ Testing.py
@@ -1,21 +1,27 @@
-import pygame
+# example usage of deck, hand, discard, drawing
+from card import Card
+from deck import Deck
 
-pygame.init()
+deck = Deck()
+hand = []
+discard = []
 
-x = 1920
-y = 1000
+drawn_cards = deck.drawN(5) # returns list
+hand.extend(drawn_cards)
 
-display_surface = pygame.display.set_mode((x,y))
-
-sword = pygame.image.load("sword.png")
-
-pygame.mouse.set_visible(True)
-
-# Game Loop
-isRunning = True
-while isRunning:
-    display_surface.blit(sword, (0,0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            isRunning = False
-        pygame.display.update()
+while (True):
+    while (len(hand) > 0):
+        discard.append(hand.pop(0)) # Simulate using/removing cards from hand
+        
+    hand.extend(deck.drawN(5)) # may be smaller than 5 if less than 5 cards remain
+    num_drawn = len(hand)
+    
+    # Put discard pile back into deck
+    if (num_drawn < 5):
+        deck.merge(discard)
+        discard = []
+        
+        # Cards left to draw
+        to_draw = 5 - num_drawn
+        for i in range(to_draw):
+            hand.extend(deck.drawN(to_draw))
---
+++ deck.py
@@ -0,0 +1,51 @@
+from card import Card
+import random
+
+
+def get_card_type(n: int, deck: {str:int}):
+    for key in deck:
+        if deck[key] >= n:
+            return key
+        n -= deck[key]
+
+class Deck:
+    def __init__(self):
+        # Maps types of Cards to how many of each remain in the deck
+        deck = dict()
+        deck['Attack'] = 5
+        deck['Defend'] = 3
+        deck['Draw'] = 2
+        self.deck = deck
+    
+    
+    # Number of remaining cards in the deck
+    def __len__(self):
+        return sum(self.deck.values())
+    
+    
+    def draw(self):
+        # Determine what kind of Card will be drawn
+        num_remaining = len(self)
+        if (num_remaining == 0):
+            return
+        
+        rand = random.randint(1, numRemaining)
+        card_type = get_card_type(rand, self.deck)
+        self.deck(card_type) -= 1
+        
+        # Create and return the appropriate Card
+        return Card(card_type)
+    
+    
+    def drawN(self, num_cards: int):
+        cards = []
+        for i in range(min(num_cards, len(self))):
+            cards.append(self.draw)
+            
+        return cards
+    
+    
+    def merge(self, cards: [Card]):
+        for card in cards:
+            card_type = card.type
+            self.deck[card_type] += 1
