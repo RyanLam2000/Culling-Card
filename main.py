c89b2a591c7d21ed469779937f79da55fd9a23e7
jhbui1 <jhbui1@uci.edu>
2020-02-01 10:43:14 -0800

    fixed cursor compilation error

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from pygame.compat import geterror
 from card import Card
 from hero import Hero
 from enemy import Enemy
+from helpers import *
 
-from Cursor import Cursor
 
 
 def load_sound(name):
@@ -34,54 +34,6 @@ def load_sound(name):
     return
 
 
-# classes for our game objects
-class Chimp(pygame.sprite.Sprite):
-    """moves a monkey critter across the screen. it can spin the
-       monkey when it is punched."""
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)  # call Sprite intializer
-        self.image, self.rect = load_image("sword.png", -1)
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.rect.topleft = 10, 10
-        self.move = 9
-        self.dizzy = 0
-
-    def update(self):
-        """walk or spin, depending on the monkeys state"""
-        if self.dizzy:
-            self._spin()
-        else:
-            self._walk()
-
-    def _walk(self):
-        """move the monkey across the screen, and turn at the ends"""
-        newpos = self.rect.move((self.move, 0))
-        if not self.area.contains(newpos):
-            if self.rect.left < self.area.left or self.rect.right > self.area.right:
-                self.move = -self.move
-                newpos = self.rect.move((self.move, 0))
-                self.image = pygame.transform.flip(self.image, 1, 0)
-            self.rect = newpos
-
-    def _spin(self):
-        """spin the monkey image"""
-        center = self.rect.center
-        self.dizzy = self.dizzy + 12
-        if self.dizzy >= 360:
-            self.dizzy = 0
-            self.image = self.original
-        else:
-            rotate = pygame.transform.rotate
-            self.image = rotate(self.original, self.dizzy)
-        self.rect = self.image.get_rect(center=center)
-
-    def punched(self):
-        """this will cause the monkey to start spinning"""
-        if not self.dizzy:
-            self.dizzy = 1
-            self.original = self.image
 
 
 def main():
@@ -92,7 +44,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((500, 500))
     pygame.display.set_caption("Culling Card")
-    pygame.mouse.set_visible(0)
+    pygame.mouse.set_visible(1)
 
     # Create The Backgound
     background = pygame.Surface(screen.get_size())
@@ -112,12 +64,9 @@ def main():
 
     # Prepare Game Objects
     clock = pygame.time.Clock()
-
-    chimp = Chimp()
     hero = Hero()
     enemy = Enemy()
-    all_cards = pygame.sprite.RenderPlain((chimp,fist,hero,enemy))
-    cursor = Cursor()
+    all_cards = pygame.sprite.RenderPlain((hero,enemy))
 
     cards = pygame.sprite.RenderPlain()
     for i in range(0,5): 
