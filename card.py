ae1af9882593be236088f813376ee03acbf45ac0
RyanLam2000 <itsryanlam2000@gmail.com>
2020-02-01 18:48:38 -0800

    This is Bryant

--- card.py
+++ card.py
@@ -20,6 +20,12 @@ class Card(pygame.sprite.Sprite):
             self.rect.topleft = (self.rect.topleft[0],self.rect.topleft[1]-20) 
             self.selected = True
             
+    
+    def set_slot(self, slot: int):
+        self.slot = slot
+        self.update()
+    
+            
     def update(self): 
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
@@ -32,6 +38,6 @@ class Card(pygame.sprite.Sprite):
  
         #set position = width * slot
         self.card_width = self.image.get_size()[0];
-        self.rect.topleft = (screen.get_width()*.2)+self.card_width*self.slot + self.slot*10, screen.get_size()[1]-(self.image.get_height()*1.3)
+        self.rect.topleft = (screen.get_width()*.2)+(self.card_width+10)*self.slot, screen.get_size()[1]-(self.image.get_height()*1.3)
         self.selected = False
        
