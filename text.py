796491aaa33f99ab2ead9583c9d77cafcd26275c
jhbui1 <jhbui1@uci.edu>
2020-02-02 05:04:34 -0800

    text class

---
+++ text.py
@@ -0,0 +1,23 @@
+class Text:
+#     render_text("Full Screen",width/2,height/2,screen,font,white,black)
+    def __init__(self,text,x,y,screen,font,c1,c2):
+        self.text = text
+        self.c1 = c1
+        self.c2 = c2
+        self.screen = screen
+        self.x = x 
+        self.y = y 
+        self.font = font
+        self.rect = None
+        self.update(screen)
+    
+    def update(self,screen):
+        
+        width = screen.get_width()/2
+        height = screen.get_height()/2
+        print(str(width)+" "+str(height))
+        text = self.font.render(self.text,True,self.c1,self.c2)
+        text_rect = text.get_rect(center=(self.x+width,self.y+height))
+        screen.blit(text,text_rect)
+        self.rect=text_rect
+    
